16d4de5a89467309659a3e6bb9f23f9ee83f5c21
Adam Doyle <adam@doordesk.net>
2025-05-29 21:47:11 -0400

    markdown tweaks

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,15 +1,25 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
+    bullet = {
+      left_pad = 2,
+    },
     completions = {
       blink = {
         enabled = true,
       },
+    },
+    heading = {
+      border = true,
+    },
+    indent = {
+      enabled = true,
+    },
+    paragraph = {
+      indent = 2,
     },
   },
 }
